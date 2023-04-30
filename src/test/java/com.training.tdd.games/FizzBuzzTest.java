be76d992359fde866a3a0afeb5379ba59b39d56c
Muniyappasamy <samynitt88@gmail.com>
2023-04-30 14:42:41 +0530

    Feat: Feature 1 - Basic Fizz Buzz Scoring with test code optimization

--- src/test/java/com.training.tdd.games/FizzBuzzTest.java
+++ src/test/java/com.training.tdd.games/FizzBuzzTest.java
@@ -1,20 +1,25 @@
 package com.training.tdd.games;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class FizzBuzzTest{
-
+    FizzBuzz fizzBuzz;
+    @BeforeEach
+    void setup(){
+        fizzBuzz = new FizzBuzz();
+    }
     @Test
     public void fizzBuzzConstructorTest(){
-        FizzBuzz fizzBuzz = new FizzBuzz();
+
         Assertions.assertNotNull(fizzBuzz);
     }
     @Test
     public void fizzBuzzReturnSameNumber(){
-        FizzBuzz fizzBuzz = new FizzBuzz();
+
         int input = 1;
         int output = fizzBuzz.returnSameNumber(input);
         Assertions.assertEquals(output,input);
