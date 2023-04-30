56a780721f81623b5c11d59d3ed8fd3d59746e3f
Muniyappasamy <samynitt88@gmail.com>
2023-04-30 15:04:49 +0530

    Feat: Feature 1 - Basic Fizz Buzz Scoring :Multiples of Three Return Fizz

--- src/test/java/com.training.tdd.games/FizzBuzz.java
+++ src/test/java/com.training.tdd.games/FizzBuzz.java
@@ -4,4 +4,10 @@ public class FizzBuzz {
     public int returnSameNumber(int input) {
         return input;
     }
+
+    public String playFizzBuzz(int input) {
+
+        String result = (input%3 == 0)? "fizz":String.valueOf(input);
+        return result;
+    }
 }
--- src/test/java/com.training.tdd.games/FizzBuzzTest.java
+++ src/test/java/com.training.tdd.games/FizzBuzzTest.java
@@ -2,6 +2,7 @@ package com.training.tdd.games;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -14,17 +15,29 @@ public class FizzBuzzTest{
     void setup(){
         fizzBuzz = new FizzBuzz();
     }
+    @DisplayName("Check fizz buzz class is created")
     @Test
     public void fizzBuzzConstructorTest(){
 
         Assertions.assertNotNull(fizzBuzz);
     }
+    @DisplayName("Display the Same Number As input")
     @ParameterizedTest
     @ValueSource( ints = {1,2,4})
     public void fizzBuzzReturnSameNumber(int input){
-        
+
         int output = fizzBuzz.returnSameNumber(input);
         Assertions.assertEquals(output,input);
     }
+
+    @DisplayName("Multiple of 3 give fizz")
+    @Test
+    public void multipleOfThreeReturnFizz(){
+        int input = 3;
+        String expectedOutput = "fizz";
+        String result = fizzBuzz.playFizzBuzz(input);
+        Assertions.assertEquals(expectedOutput,result);
+    }
+   
 
 }
